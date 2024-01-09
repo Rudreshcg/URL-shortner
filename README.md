4ff5a84b1077c2e381d2a03f8b3e0f7ab127533f
Rudreshcg <rudreshcsbg82@gmail.com>
2024-01-09 22:14:50 +0530

    first commit for portfolio

---
+++ README.md
@@ -0,0 +1,4 @@
+# personalportfolio
+My personal portfolio from complete web development.
+
+https://rashmitha-here.github.io/personalportfolio/
---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["And I'm a B.Tech CSE Final Year Student", "I'm Interested in Front-End WebDevelopement", "I'm Interested in Coding", "I'm Interested in Problem Solving..." ],
+    typeSpeed: 90,
+    backSpeed: 70,
+    backDelay: 50,
+    loop: true
+});
+
