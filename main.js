9afe000885da872b3c5d6dfd8617186147434091
Rudresh cg <108670628+Rudreshcg@users.noreply.github.com>
2024-03-16 12:25:04 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["And I'm a B.Tech CSE Final Year Student", "I'm Interested in Front-End WebDevelopement", "I'm Interested in Coding", "I'm Interested in Problem Solving..." ],
-    typeSpeed: 90,
-    backSpeed: 70,
-    backDelay: 50,
-    loop: true
-});
-
